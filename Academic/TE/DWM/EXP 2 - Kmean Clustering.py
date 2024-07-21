597e4feb9e17c11276db7b75c1ed090a13b2dd24
Soham Mandavkar <radicalghost98@gmail.com>
2024-07-21 21:50:52 +0530

    new repo

---
+++ Academic/TE/DWM/EXP 2 - Kmean Clustering.py
@@ -0,0 +1,38 @@
+import random as rd
+import csv
+
+def diff(a, b):
+    return abs(a - b)
+
+def assign_cluster(string, centroid):
+    clusters = [[] for i in range(len(centroid))]
+    for i in string:
+        dist = [diff(i, j) for j in centroid]
+        min_dist = dist.index(min(dist))
+        clusters[min_dist].append(i)
+    return clusters
+
+def Kmean(string, no_clusters, max_iter = 100):
+    centroid = rd.sample(string, no_clusters)
+    for i in range(max_iter):
+        clusters = assign_cluster(string, centroid)
+        clusters = [sorted(cluster) for cluster in clusters]
+        new_centroid = [sum(i) / len(i) for i in clusters]
+        if new_centroid == centroid:
+            break
+        centroid = new_centroid
+    return clusters, centroid
+
+
+# Input
+with open("input.csv", "r") as file:
+    reader = csv.reader(file)
+    string = [int(number) for row in reader for number in row]
+
+no_clusters = int(input("Enter no. of cluster: "))
+sorted(string)
+
+clusters , centroid = Kmean(string, no_clusters)
+
+print("Clusters: ", sorted(clusters))
+print("Centroids: ", sorted(centroid))
